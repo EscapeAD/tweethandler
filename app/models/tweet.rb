d3037495d49ef5b5f5c2c44e6643b9c8452c4e17
Adam Tse <mr.adam.tse@gmail.com>
2017-03-24 16:52:40 -0400

    delete first row header

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,9 +4,11 @@ class Tweet < ApplicationRecord
   def self.import(file)
     # default CSV format not UTF-8 based on example
     sheet = Roo::CSV.new(file.path, csv_options: {external_encoding: 'ISO-8859-1', internal_encoding: 'UTF-8'})
+    sheet.parse(clean: true)
     sheet.each(tweet: 'Tweet_Text', type: 'Type') do |hash|
     Tweet.create!(tweet: hash[:tweet], tweet_type: hash[:type])
     end
+    Tweet.first.delete
   end
 
 end
